e5536965e2d95f4cac957cdf74c37ff3d8071ae7
Deepak Kumar <deepak748797@gmail.com>
2022-07-13 00:39:18 +0530

    installed router module and created basic routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        The Home Page
+      </Route>
+      <Route exact path="/About">
+        The About Page
+      </Route>
+      <Route>Error 404 Page Not Found</Route>
+    </Switch>
+  );
 }
 
 export default App;
