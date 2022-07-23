607f8f440d5a0c0e107b11a986e3b951e4d8f8a5
Deepak Kumar <deepak748797@gmail.com>
2022-07-23 23:16:34 +0530

    added the lastQuery custom hook

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,9 +3,10 @@ import ActorGrid from '../Components/Actor/ActorGrid';
 import MainPageLayout from '../Components/MainPageLayout';
 import ShowGrid from '../Components/Show/ShowGrid';
 import apiGet from '../misc/config';
+import { useLastQuery } from '../misc/Custom-hooks';
 
 export default function Home() {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useLastQuery();
   const [searchResult, setSearchResult] = useState(null);
   const [searchOption, setSearchOption] = useState('shows');
   const onInputChange = event => {
